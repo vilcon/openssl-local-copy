openssl/openssl

Add RFC8701 GREASE Support
This adds support for RFC8701, which can be used in testing to ensure
TLS extensibility and proper processing. This is different from fuzzing
in that this can be done on both the client and the server, and
allows the connection to complete.

The API is public to allow end-users to do their own testing with GREASE.

test/recipes/90-test_grease.t
@@ -1,0 +1,21 @@
+#! /usr/bin/env perl
+# Copyright 2016-2022 The OpenSSL Project Authors. All Rights Reserved.
+#
+# Licensed under the Apache License 2.0 (the "License").  You may not use
+# this file except in compliance with the License.  You can obtain a copy
+# in the file LICENSE in the source distribution or at
+# https://www.openssl.org/source/license.html
+
+use OpenSSL::Test::Utils;
+use OpenSSL::Test qw/:DEFAULT srctop_dir bldtop_dir/;
+
+BEGIN {
+    setup("test_grease");
+}
+
+use lib srctop_dir('Configurations');
+use lib bldtop_dir('.');
+
+plan tests => 1;
+
+ok(run(test(["greasetest", srctop_dir("test", "certs")])), "running greasetest");

vilcon/openssl-local-copy

test/recipes/90-test_grease.t
@@ -1,0 +1,21 @@
+#! /usr/bin/env perl
+# Copyright 2016-2022 The OpenSSL Project Authors. All Rights Reserved.
+#
+# Licensed under the Apache License 2.0 (the "License").  You may not use
+# this file except in compliance with the License.  You can obtain a copy
+# in the file LICENSE in the source distribution or at
+# https://www.openssl.org/source/license.html
+
+use OpenSSL::Test::Utils;
+use OpenSSL::Test qw/:DEFAULT srctop_dir bldtop_dir/;
+
+BEGIN {
+    setup("test_grease");
+}
+
+use lib srctop_dir('Configurations');
+use lib bldtop_dir('.');
+
+plan tests => 1;
+
+ok(run(test(["grease_test", srctop_dir("test", "certs")])), "running grease_test");