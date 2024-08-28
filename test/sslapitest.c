openssl/openssl

Fixed memory leak

test/sslapitest.c
@@ -1883,7 +1883,7 @@ static int ocsp_server_cb_single(SSL *s, void *arg)
         return SSL_TLSEXT_ERR_ALERT_FATAL;
     }
 
-    OPENSSL_free(respder);
+    OPENSSL_free(ocspresp);
 
     ocsp_server_called = 1;
     return SSL_TLSEXT_ERR_OK;

vilcon/openssl-local-copy

test/sslapitest.c
@@ -1884,6 +1884,8 @@ static int ocsp_server_cb_single(SSL *s, void *arg)
         return SSL_TLSEXT_ERR_ALERT_FATAL;
     }
 
+    OPENSSL_free(respder);
+
     ocsp_server_called = 1;
     return SSL_TLSEXT_ERR_OK;
 }